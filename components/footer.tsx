6d049a6b0b0715f605147261ad0de9600ca41303
wiz-labs1 <wiz.labs.1@gmail.com>
2025-09-19 20:35:49 +0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -145,7 +145,7 @@ export function Footer() {
 
             <div className="flex items-center gap-4">
               <div className="flex items-center gap-1 text-sm text-muted-foreground">
-                Dev Studios <span className="text-xs">™</span> 2023
+               Wiz Dev Studios <span className="text-xs">™</span> 2023
               </div>
             </div>
 
